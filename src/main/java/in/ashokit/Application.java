7eec1e182beb305f11d7e88b788037f45696005b
kanhereanku09 <ankitajige9@gmail.com>
2025-04-17 12:35:17 +0530

    first comment

---
+++ src/main/java/in/ashokit/Application.java
@@ -0,0 +1,19 @@
+package in.ashokit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+
+    int a = 10;
+    int b =20;
+     int c = 30 ;
+     String s = "ankita"
+	}
+
+}
